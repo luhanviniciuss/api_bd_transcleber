cdb4c6cdb391edeb60f1615374cc47eccc94e53b
Luhan Vinicius <luhan.vinicius@jblogistica.local>
2026-05-13 08:15:48 -0300

    fix: support both DB_PASS and DB_PASSWORD names

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ const db = knex({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASS || process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME,
   },
   pool: { min: 0, max: 10 }
